7a8a08662a23f70082cd8597f6630c81e1a86cdc
coeevee <120228155+coeevee@users.noreply.github.com>
2025-02-11 13:54:19 -0700

    Add files via upload

---
+++ #is being of the code.py
@@ -0,0 +1,14 @@
+print("this is a madlib")
+print("this will be a funny story")
+#inputs for the words
+noun1=input("enter a persons name ")
+noun2=input("enter a place ")
+verb1=input("enter a a form of movement ")
+verb2=input("enter a form of movement ")
+adjective1=input("enter an adjective ")
+adjective2=input("enter an adjective ")
+#adds all the variables to the story
+print("the "+noun1+" "+verb1+" to the "+noun2+" via "+verb2+" the "+noun2+" is "+adjective1+" and "+adjective2)
+print("the end")
+# the end of the story
+#
